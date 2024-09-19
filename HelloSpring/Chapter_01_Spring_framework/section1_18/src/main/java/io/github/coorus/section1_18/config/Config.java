23c772f24c767014a337751aa4be5803797e9b84
Sylvan <sylvansora@gmail.com>
2024-09-19 17:33:18 +0800

    Update

---
+++ HelloSpring/Chapter_01_Spring_framework/section1_18/src/main/java/io/github/coorus/section1_18/config/Config.java
@@ -0,0 +1,22 @@
+package io.github.coorus.section1_18.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class Config {
+    @Bean
+    public DataSource dataSource(DataSourceProperties properties) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(properties.getDriverClassName());
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+        return dataSource;
+
+    }
+}
---
+++ HelloSpring/Chapter_01_Spring_framework/section1_19/src/main/java/io/github/coorus/section1_19/Section119Application.java
@@ -0,0 +1,13 @@
+package io.github.coorus.section1_19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section119Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section119Application.class, args);
+    }
+
+}
